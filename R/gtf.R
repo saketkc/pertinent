b75af8b17bbe13dc1e45d152fff08df2d37f05b9
Saket Choudhary <saketkc@gmail.com>
2022-11-09 08:23:53 -0500

    cleanup

--- R/gtf.R
+++ R/gtf.R
@@ -10,28 +10,23 @@ ReplaceMissingEntries <- function(df, col.source, col.target) {
 #' @importFrom rtracklayer import
 #' @importFrom S4Vectors mcols splitAsList
 #' @importFrom magrittr %>%
-#' @importFrom dplyr arrange filter rename select
+#' @importFrom dplyr arrange distinct filter left_join rename select
 #' @export
 ParseGTF <- function(gtf) {
-  # gtf <- "/brahms/choudharys/data/Parsebio_Reference/Mus_musculus.GRCm39.108.gtf.gz"
   gtf.gr <- import(con = gtf, format = "gtf")
-
   gene_map <- mcols(x = gtf.gr)[, c("gene_id", "gene_name", "gene_biotype")] %>%
     as.data.frame() %>%
-    unique() %>%
+    distinct() %>%
     arrange(gene_id)
   gene_map <- ReplaceMissingEntries(gene_map, col.source = "gene_id", col.target = "gene_name")
-
   tx_map <- mcols(x = gtf.gr)[, c("transcript_id", "gene_id", "gene_name", "gene_biotype")] %>%
     as.data.frame() %>%
     filter(!is.na(transcript_id)) %>%
-    unique() %>%
+    distinct() %>%
     arrange(gene_id, transcript_id)
   tx_map <- ReplaceMissingEntries(tx_map, col.source = "gene_id", col.target = "gene_name")
-
   txdb <- suppressWarnings(expr = makeTxDbFromGFF(file = gtf, format = "gtf"))
   grl <- transcriptsBy(txdb, by = "gene")
-
   tx_lengths <- transcriptLengths(txdb = txdb, with.cds_len = TRUE, with.utr5_len = TRUE, with.utr3_len = TRUE) %>%
     rename(transcript_id = tx_name) %>%
     select(-tx_id)
@@ -39,6 +34,5 @@ ParseGTF <- function(gtf) {
     left_join(y = tx_map) %>%
     select(transcript_id, gene_id, gene_name, gene_biotype, tx_len, cds_len, utr5_len, utr3_len) %>%
     arrange(gene_name, gene_id)
-
   return(list(tx_lengths = tx_lengths, txdb = txdb))
 }
