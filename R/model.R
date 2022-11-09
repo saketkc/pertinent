25ed20ea65afb05267eaa93daf5430c82c1d7f8b
Saket Choudhary <saketkc@gmail.com>
2022-11-09 16:49:00 -0500

    Add utility methods for count modeling

--- R/model.R
+++ R/model.R
@@ -1,95 +1,77 @@
-#' SCT's reconstruction error
-#' @param seu Seurat object
+#' Estimate phi based on a linear fit to mean-variance relationship
 #' @export
-
-SCTReconstructionError <- function(seu, dims = 1:30, nfeatures = 3000, idents = c("seurat_clusters"),
-                                   outdir = NULL, overwrite = TRUE) {
-  sct <- SCTransform(seu, ncells = 5000, variable.features.n = nfeatures, verbose = FALSE) %>%
-    RunPCA(verbose = FALSE) %>%
-    RunUMAP(dims = dims, verbose = FALSE) %>%
-    FindNeighbors(
-      dims = dims,
-      verbose = FALSE
-    ) %>%
-    FindClusters(verbose = FALSE)
-}
-
-
-
-#' @export
-
-fit_phi <- function(mean, variance) {
+EstimateNBPhi <- function(mean, variance) {
   # fits a linear model on variance ~ mu + phi*mu^2 to estimate phi (which is
   # 1/theta)
   var_res <- variance - mean
-  fit <- lm(var_res ~ 0 + I(mean^2))
+  fit <- lm(formula = var_res ~ 0 + I(mean^2))
   phi <- fit$coefficients[[1]]
-  return(list(phi = phi, fit = (mean + phi * mean^2)))
+  return(data.frame(phi = phi, fit = (mean + phi * mean^2)))
 }
 
-#' @import sparseMatrixStats
+#' Perform mean-variance calculation on the count matrix.
+#' @importFrom sparseMatrixStats rowMeans2 rowVars
 #' @export
+MeanVarFit <- function(counts = NULL, means = NULL, variance = NULL) {
+  if (is.null(x = counts) & ((is.null(x = means) | is.null(x = variance)))) {
+    stop("Need to specify one of counts or (means, variance).")
+  }
+  if (!is.null(x = counts)) {
+    means <- rowMeans2(x = counts)
+    variance <- rowVars(x = counts)
+  }
 
-MeanVarFit <- function(counts) {
-  means <- sparseMatrixStats::rowMeans2(counts)
-  variance <- sparseMatrixStats::rowVars(counts)
   df <- data.frame(mean = means, variance = variance, gene = rownames(counts))
-  fit <- fit_phi(means, variance)
-  phi <- fit[["phi"]]
-
-  df$fit <- fit[["fit"]]
-  df$phi <- phi
+  fit <- EstimateNBPhi(means, variance)
+  df$fit <- fit$fit
+  df$phi <- fit$phi
   df$residual <- df$variance - df$fit
   return(df)
 }
 
 
 
-
-#' @import sparseMatrixStats
+#' Plot mean variance relationship for counts or dataframe
+#' @importFrom sparseMatrixStats rowMeans2 rowVars
 #' @export
-
-MeanVarPlot <- function(counts, annotate = FALSE, logxy = F) {
-  means <- sparseMatrixStats::rowMeans2(counts)
-  variance <- sparseMatrixStats::rowVars(counts)
-  df <- data.frame(mean = means, variance = variance, gene = rownames(counts))
-  fit <- fit_phi(means, variance)
-  phi <- fit[["phi"]]
-
-  df$fit <- fit[["fit"]]
+MeanVarPlot <- function(counts = NULL, df = NULL, annotate = FALSE, logxy = F) {
+  if (is.null(x = df)) {
+    means <- rowMeans2(x = counts)
+    variance <- rowVars(x = counts)
+    df <- data.frame(mean = means, variance = variance, gene = rownames(counts))
+  }
+  fit <- EstimateNBPhi(df$mean, df$variance)
+  phi <- fit$phi
+  df$fit <- fit$fit
 
   # this is (mu+mu^2*phi)/mu-1
   genewise_phi <- (variance / means - 1) / means
-
-  compare_phi <- genewise_phi / fit[["phi"]]
+  compare_phi <- genewise_phi / fit$phi
   compare_phi[is.na(compare_phi)] <- 0
-
   index <- genewise_phi > quantile(genewise_phi, 0.999, na.rm = T)[[1]]
   index[is.na(index)] <- FALSE
-
 
   df$label <- ""
   df$label[index] <- df$gene[index]
 
   p <- ggplot(df, aes(x = mean, y = variance, color = "d", label = label)) +
-    # geom_point(
-    #  alpha = 0.5,
-    #  shape = 16, color = "black"
-    # )
-    scattermore::geom_scattermore(alpha = 0.5)
+    geom_point(
+      alpha = 0.1,
+      shape = 16,
+      color = "black"
+    )
+
   if (annotate) {
     p <- p + geom_text_repel()
   }
   p <- p + geom_line(aes(x = mean, y = mean, color = "a"), size = 1.2, show.legend = T) +
     geom_line(aes(x = mean, y = fit, color = "b"), size = 1.2, show.legend = T) +
     xlab("Mean") + ylab("Variance") + ggtitle("Mean-var")
-  p <- p + scale_color_manual(name = "", values = c(
-    a = "red", b = "blue",
-    d = "black"
-  ), labels = c("Poisson-fit", paste0("NB-fit (phi=", round(
-    phi,
-    1
-  ), ")"), "Gene")) + theme(legend.position = "bottom")
+  p <- p + scale_color_manual(
+    name = "", values = c(a = "#e41a1c", b = "#377eb8", d = "black"),
+    labels = c("Poisson-fit", paste0("NB-fit (phi=", round(phi, 1), ")"), "Gene")
+  ) +
+    theme(legend.position = "bottom")
   if (logxy) {
     p <- p + scale_x_log10() +
       scale_y_log10()
@@ -97,19 +79,19 @@ MeanVarPlot <- function(counts, annotate = FALSE, logxy = F) {
   return(p)
 }
 
-#' @import sparseMatrixStats
+#' @importFrom sparseMatrixStats rowMeans2 rowVars
 #' @export
 
-DropoutPlot <- function(counts, annotate = F) {
+DropoutPlot <- function(counts = NULL, annotate = FALSE) {
   n_cells <- dim(counts)[2]
-  means <- sparseMatrixStats::rowMeans2(counts)
-  variance <- sparseMatrixStats::rowVars(counts)
+  means <- rowMeans2(x = counts)
+  variance <- rowVars(x = counts)
   dropouts.df <- data.frame(
     mean = means,
     obs_dropout = apply(counts == 0, 1, sum) / n_cells,
     gene = rownames(counts)
   )
-  fit <- fit_phi(means, variance)
+  fit <- EstimateNBPhi(means, variance)
   phi <- fit[["phi"]]
 
   dropouts.df$nb_expected_dropout <- (1 / (phi * means + 1))^(1 / phi)
@@ -123,7 +105,6 @@ DropoutPlot <- function(counts, annotate = F) {
 
   p_counts <- ggplot(dropouts.df, aes(x = mean, y = obs_dropout, color = "d", label = label)) +
     geom_point(alpha = 0.5)
-  # scattermore::geom_scattermore(alpha=0.5, )
   if (annotate) {
     p_counts <- p_counts + geom_text_repel()
   }
@@ -143,12 +124,12 @@ DropoutPlot <- function(counts, annotate = F) {
 
 fit_glmGamPoiOnlyInterceptSingleGene <- function(umi) {
   model_str <- "~ 1"
-  fit <- glmGamPoi::glm_gp(
+  fit <- glm_gp(
     data = umi, design = as.formula(gsub("y", "", model_str)),
     size_factors = FALSE
   )
   fit$theta <- pmin(1 / fit$overdispersions, rowMeans(fit$Mu) / 1e-04)
-  colnames(fit$Beta)[1] <- "b0"
+  colnames(x = fit$Beta)[1] <- "b0"
   mean <- mean(umi)
   variance <- var(umi)
   overdispersion.factor <- variance / mean
@@ -189,7 +170,7 @@ getDispersionsOnlyIntercept <- function(counts) {
 
 fit_glmGamPoiWithLibsizeSingleGene <- function(umi, data) {
   model_str <- "~ log_umi"
-  fit <- glmGamPoi::glm_gp(
+  fit <- glm_gp(
     data = umi, design = as.formula(gsub("y", "", model_str)),
     col_data = data, size_factors = FALSE
   )
@@ -199,11 +180,8 @@ fit_glmGamPoiWithLibsizeSingleGene <- function(umi, data) {
   fit.df <- cbind(fit$theta, fit$Beta, fit$overdispersions)
   colnames(fit.df)[1] <- "theta"
   colnames(fit.df)[4] <- "overdispersion"
-  # means <- mean(umi) variance <- var(umi)
 
-  # fit <- fit_phi(means, variance) phi <- (variance - means)/(means^2)#
-  # fit[['phi']] fit.df$phi <- phi
-  return(as.data.frame(fit.df))
+  return(as.data.frame(x = fit.df))
 }
 
 
