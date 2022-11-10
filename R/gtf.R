ec8766216b849cbb70b93df539cee75c54029604
Saket Choudhary <saketkc@gmail.com>
2022-11-10 11:15:27 -0500

    Retain strand information

--- R/gtf.R
+++ R/gtf.R
@@ -9,28 +9,42 @@ ReplaceMissingEntries <- function(df, col.source, col.target) {
 #' @importFrom GenomicFeatures  makeTxDbFromGFF transcriptsBy transcriptLengths
 #' @importFrom rtracklayer import
 #' @importFrom S4Vectors mcols splitAsList
-#' @importFrom GenomeInfoDb seqlevels
+#' @importFrom GenomeInfoDb
+#' seqinfo seqinfo<-
+#' seqnames seqnames<-
+#' seqlevels seqlevels<-
+#' sortSeqlevels
+#' seqlengths seqlengths<-
+#' isCircular isCircular<-
+#' genome genome<-
 #' @importFrom magrittr %>%
+#' @importFrom BiocGenerics strand
 #' @importFrom dplyr arrange distinct filter left_join rename select
 #' @export
 ParseGTF <- function(gtf, seqlevels_prefix = NULL) {
   gtf.gr <- import(con = gtf, format = "gtf")
-  if (!is.null(x = seqlevels_prefix)){
+  if (!is.null(x = seqlevels_prefix)) {
     seqlevels(x = gtf.gr) <- paste0(seqlevels_prefix, seqlevels(x = gtf.gr))
   }
-  gene_map <- mcols(x = gtf.gr)[, c("gene_id", "gene_name", "gene_biotype")] %>%
-    as.data.frame() %>%
+  gene_map <- mcols(x = gtf.gr)[, c("gene_id", "gene_name", "gene_biotype")] %>% as.data.frame()
+  gene_map$strand <- as.character(x = strand(x = gtf.gr))
+  gene_map <- gene_map %>%
     distinct() %>%
     arrange(gene_id)
+  gene_map$strand[gene_map$strand == "+"] <- 1
+  gene_map$strand[gene_map$strand == "-"] <- -1
   gene_map <- ReplaceMissingEntries(gene_map, col.source = "gene_id", col.target = "gene_name")
-  tx_map <- mcols(x = gtf.gr)[, c("transcript_id", "gene_id", "gene_name", "gene_biotype")] %>%
-    as.data.frame() %>%
+  tx_map <- mcols(x = gtf.gr)[, c("transcript_id", "gene_id", "gene_name", "gene_biotype")] %>% as.data.frame()
+  tx_map$strand <- as.character(x = strand(x = gtf.gr))
+  tx_map <- tx_map %>%
     filter(!is.na(transcript_id)) %>%
     distinct() %>%
     arrange(gene_id, transcript_id)
+  tx_map$strand[tx_map$strand == "+"] <- 1
+  tx_map$strand[tx_map$strand == "-"] <- -1
   tx_map <- ReplaceMissingEntries(tx_map, col.source = "gene_id", col.target = "gene_name")
   txdb <- suppressWarnings(expr = makeTxDbFromGFF(file = gtf, format = "gtf"))
-  if (!is.null(x = seqlevels_prefix)){
+  if (!is.null(x = seqlevels_prefix)) {
     seqlevels(x = txdb) <- paste0(seqlevels_prefix, seqlevels(x = txdb))
   }
   grl <- transcriptsBy(txdb, by = "gene")
@@ -38,8 +52,8 @@ ParseGTF <- function(gtf, seqlevels_prefix = NULL) {
     rename(transcript_id = tx_name) %>%
     select(-tx_id)
   tx_lengths <- tx_lengths %>%
-    left_join(y = tx_map) %>%
-    select(transcript_id, gene_id, gene_name, gene_biotype, tx_len, cds_len, utr5_len, utr3_len) %>%
+    left_join(y = tx_map, by = c("transcript_id", "gene_id")) %>%
+    select(transcript_id, gene_id, gene_name, gene_biotype, strand, tx_len, cds_len, utr5_len, utr3_len) %>%
     arrange(gene_name, gene_id)
   return(list(tx_lengths = tx_lengths, txdb = txdb, tx = grl))
 }
--- R/visualization.R
+++ R/visualization.R
@@ -3,6 +3,7 @@
 #' @importFrom wiggleplotr plotCoverage getGenotypePalette
 #' @importFrom dplyr select filter pull arrange desc
 #' @importFrom GenomicFeatures exonsBy
+#' @export
 VisualizeWigCoverage <- function(track.data, gene.name, gtf.data = NULL, gtf = NULL, exons.gr = NULL, tx.ids = NULL, ...) {
   if (is.null(x = gtf.data)) {
     # do some processing to get exons.gr
@@ -18,7 +19,8 @@ VisualizeWigCoverage <- function(track.data, gene.name, gtf.data = NULL, gtf = N
   tx_of_interest <- unique(x = tx.gr.unlist$tx_name)
   exons.gr <- exonsBy(x = gtf.data[["txdb"]], by = "tx", use.names = TRUE)[tx_of_interest]
 
-  p <- suppressWarnings(expr = plotCoverage(exons.gr,
+  p <- suppressWarnings(expr = plotCoverage(
+    exons = exons.gr, transcript_annotations = tx.lengths,
     track_data = track.data,
     heights = c(2, 1),
     fill_palette = getGenotypePalette(),
