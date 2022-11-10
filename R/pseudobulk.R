15557b3a5a6a9ef7862d6edb11bc87b3801d0439
Saket Choudhary <saketkc@gmail.com>
2022-11-10 02:11:53 -0500

    Add pseudobulk method for counts

--- R/pseudobulk.R
+++ R/pseudobulk.R
@@ -13,47 +13,78 @@ DistToDF <- function(inDist) {
   )
 }
 
-#' Create a pseudobulk of any seurat object with replicates
-#' @importFrom Seurat AddMetaData AggregateExpression NormalizeData
-#'
+
+#' Create a pseudobulk of input matrix given cell.group
+#' @importFrom stringr str_split_fixed
+#' @importFrom SeuratObject CreateSeuratObject
+#' @importFrom magrittr  %>%
+#' @importFrom dplyr group_by pull bind_rows row_number
+#' @importFrom tidyr drop_na nest
+#' @importFrom tibble as_tibble
+#' @importFrom Matrix sparse.model.matrix
+#' @param counts A sparse matrix
+#' @param cell.group A vector of assigned group/group (one per cell)
+#' @param k.replicates Number of replicates
+#' @param seed Integer seed
+#' @param return.seurt bool Whether seurat object should be returned
+#' @examples
+#' data("pbmc_small")
+#' counts <- GetAssayData(object = pbmc_small, assay = "RNA", slot = "counts")
+#' cell.group <- pbmc_small$groups
+#' k.replicates <- 3
+#' counts.agg <- CreateKPseudoBulk(counts = counts, cell.group = cell.group, k.replicates = k.replicates)
 #' @export
-CreateKPseudoBulk <- function(object, ident.name = "subclass.predicted.id", k_replicates = 3, seed = 42) {
-  set.seed(seed)
-  metadata <- object@meta.data
-  pred_celltypes <- metadata[[ident.name]]
-  celltype_composition <- table(pred_celltypes)
-  replicate_labels <- paste0("replicate", seq(k_replicates))
-
-  generated_labels <- lapply(names(celltype_composition),
-    FUN = function(x) sample(replicate_labels, size = celltype_composition[[x]], replace = TRUE)
+CreateKPseudoBulk <- function(counts,
+                              cell.group,
+                              k.replicates = 6,
+                              seed = 42,
+                              return.seurat = FALSE) {
+  set.seed(seed = seed)
+  metadata <- data.frame(
+    barcode = colnames(counts),
+    group = as.character(cell.group)
   )
-  names(generated_labels) <- names(celltype_composition)
+  metadata_group <- list()
 
-  metadata$replicate <- ""
-  for (celltype in names(generated_labels)) {
-    metadata_sub <- metadata[metadata[[ident.name]] == celltype, ]
-    metadata[rownames(metadata_sub), "replicate"] <- generated_labels[[celltype]]
+  for (group in setdiff(x = unique(x = metadata$group), y = NA)) {
+    metadata.subset <- metadata[metadata$group == group, ] %>%
+      drop_na() %>%
+      as_tibble()
+    # shuffle the rows just in case they were ordered somehow earlier
+    metadata.subset <- metadata.subset[sample(nrow(metadata.subset)), ]
+    partitioned_data <- metadata.subset %>%
+      group_by((row_number() - 1) %/% (n() / k.replicates)) %>%
+      nest() %>%
+      pull(data)
+    names(partitioned_data) <- paste0("replicate", names(partitioned_data))
+    metadata_joined <- bind_rows(partitioned_data, .id = "replicate") %>% drop_na()
+    metadata_group[[group]] <- metadata_joined
   }
-  # for each celltype generate labels for each cell from replicate_labels such that
-  # each celltype gets similar number of celltype labels
-
-  composition <- as.data.frame(table(metadata[[ident.name]], metadata$replicate))
-  colnames(composition) <- c("celltype", "replicate", "ncells")
-
-  object <- AddMetaData(object, metadata = metadata)
-  Idents(object) <- ident.name
-  agg_obj <- AggregateExpression(object, assays = "RNA", slot = "counts", group.by = c(ident.name, "replicate"), return.seurat = T)
-  metadata_celltypes <- stringr::str_split_fixed(colnames(agg_obj), "_", 2)[, 1]
-  metadata_replicates <- stringr::str_split_fixed(colnames(agg_obj), "_", 2)[, 2]
-  metadata <- data.frame(celltype = metadata_celltypes, replicate = metadata_replicates, sample_name = colnames(agg_obj))
-  metadata <- as.data.frame(dplyr::left_join(metadata, composition, by = c("celltype", "replicate")))
-  rownames(metadata) <- metadata$sample_name
-
-  agg_obj <- AddMetaData(agg_obj, metadata = metadata)
-  agg_obj <- NormalizeData(agg_obj)
-
-
-  return(agg_obj)
+  metadata_final <- bind_rows(metadata_group) %>% as.data.frame()
+  rownames(metadata_final) <- metadata_final$barcode
+  mymodel.matrix <- sparse.model.matrix(
+    object = ~ 0 + replicate:group,
+    data = metadata_final
+  )
+  colnames(x = mymodel.matrix) <- sapply(
+    X = colnames(x = mymodel.matrix),
+    FUN = function(name) {
+      name <- gsub(pattern = "group", replacement = "", x = name)
+      return(paste0(rev(x = unlist(x = strsplit(x = name, split = ":"))),
+        collapse = "__"
+      ))
+    }
+  )
+  counts.use <- counts[, metadata_final$barcode]
+  counts.agg <- as.matrix(x = (counts.use %*% mymodel.matrix))
+  if (return.seurat) {
+    object <- CreateSeuratObject(counts = counts.agg)
+    metadata_split <- str_split_fixed(string = colnames(x = counts.agg), pattern = "__", n = 2)
+    object$group <- metadata_split[, 1]
+    object$replicate <- metadata_split[, 2]
+    return(object)
+  }
+  return(counts.agg)
 }
 
 #' Calculate average distance given a set of replicate distances
