28dc80b4d2db6cb6fd81f04bf3cf9b63404f3f63
Saket Choudhary <saketkc@gmail.com>
2022-12-20 16:07:17 -0500

    Remove SeuratDisk from hard requirements

--- R/export_h5ad.R
+++ R/export_h5ad.R
@@ -2,8 +2,6 @@
 #'
 #' @param filepath Path to RDS file
 #' @importFrom Seurat DietSeurat GetAssayData SetAssayData
-#' @importFrom  SeuratDisk SaveH5Seurat Convert
-#'
 #' @export
 RDStoH5AD <- function(filepath) {
   h5seurat <- gsub(".rds", ".h5Seurat", filepath)
@@ -11,6 +9,6 @@ RDStoH5AD <- function(filepath) {
   query <- DietSeurat(query, assays = "RNA", scale.data = FALSE, dimreducs = NULL, graphs = NULL)
 
   query <- SetAssayData(query, slot = "data", new.data = GetAssayData(query, slot = "counts", assay = "RNA"))
-  SaveH5Seurat(query, filename = h5seurat, overwrite = TRUE, verbose = FALSE)
-  Convert(source = h5seurat, dest = "h5ad", overwrite = TRUE, verbose = FALSE)
+  SeuratDisk::SaveH5Seurat(query, filename = h5seurat, overwrite = TRUE, verbose = FALSE)
+  SeuratDisk::Convert(source = h5seurat, dest = "h5ad", overwrite = TRUE, verbose = FALSE)
 }
